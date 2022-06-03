538366d4e318ee0f6a34c12b23be86346e3809bf
youngseoJi <estell.ys.j@gmail.com>
2022-06-04 01:20:11 +0900

    JS : navbar menu click

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar-dark");
   }
 });
+
+// navbar menu 클릭 시 해당 섹션 이동
+const navbarMenu = document.querySelector(".navbar_menu");
+navbarMenu.addEventListener("click", (e) => {
+  const target = e.target;
+  const link = target.dataset.link;
+  if (link === null) {
+    return;
+  }
+
+  console.log(e.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
